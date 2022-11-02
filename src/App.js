d4c8b18642ed016609e1ae93d5bccfdbb0f62071
Chris <chrishethertonweb@gmail.com>
2022-11-02 14:53:16 +0100

    refactored to make it have sense

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import "./App.css";
 import InputForm from './InputForm'
+import { useId } from "react";
 
 
 
 function App() {
 
+  const id = useId()
 
   return (
     <div className="App">
-        <InputForm/>
+        <InputForm label='firstName' id={id} type='text' />
+        <InputForm label='lastName'id={id} type='text' />
+        <InputForm label='email'id={id} type='email'  />
+
+
     </div>
   );
 }
--- src/InputForm.jsx
+++ src/InputForm.jsx
@@ -1,20 +1,15 @@
-import { useId } from "react";
 
 function InputForm(props) {
   // const id = "myId";
 
-  const id = useId()
+  
   // console.log(id);
 
   return (
     <div className="inputForm">
-      <label className="font-mono text-lg" htmlFor={id}>{props.label}First Name</label>
-      <input className="border-2 border-black h-[30px]  w-[300px]" placeholder="...type here"  id={id} type={props.type} />
-      <label className="font-mono text-lg" htmlFor={id+ '-lastName'}>{props.label}Last Name</label>
-      <input className="border-2 border-black h-[30px]  w-[300px]" placeholder="...type here"  id={id + '-lastName'} type={props.type} />
-      <label className="font-mono text-lg" htmlFor={id + '-email'}>{props.label}Email</label>
-      <input className="border-2 border-black h-[30px]  w-[300px]" placeholder="...type here" id={id + '-email'} type={props.type} />
-     
+      <label className="font-mono text-lg" htmlFor={`${props.id}+${props.label}`}>{props.label}</label>
+      <input className="border-2 border-black h-[30px]  w-[300px]" placeholder="...type here"  
+      id={`${props.id}+${props.label}`} type={props.type} />    
     </div>
   );
 }
