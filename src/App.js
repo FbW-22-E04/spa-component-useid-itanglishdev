fd7005728a2c7e7e61bb24bec18b635509b6615e
Chris <chrishethertonweb@gmail.com>
2022-11-02 13:40:19 +0100

    added input elements and basic css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import InputForm from './InputForm'
 
 function App() {
   return (
     <div className="App">
-        
+        <InputForm/>
     </div>
   );
 }
--- src/InputForm.jsx
+++ src/InputForm.jsx
@@ -2,8 +2,12 @@ function InputForm(props) {
   const id = "myId";
   return (
     <div className="inputForm">
-      <label htmlFor={id}>{props.label}</label>
-      <input id={id} type={props.type} />
+      <label htmlFor={id}>{props.label}First Name</label>
+      <input className="border-2 border-black h-[30px] w-[300px]" placeholder="...type here"  id={id} type={props.type} />
+      <label htmlFor={id}>{props.label}Last Name</label>
+      <input className="border-2 border-black h-[30px] w-[300px]" placeholder="...type here"  id={id} type={props.type} />
+      <label htmlFor={id}>{props.label}Email</label>
+      <input className="border-2 border-black h-[30px] w-[300px]" placeholder="...type here" id={id} type={props.type} />
     </div>
   );
 }
